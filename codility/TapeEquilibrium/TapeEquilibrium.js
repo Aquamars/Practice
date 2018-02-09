b44efe8cb49c5b70486a24d22bdde17aa0fc906e
Aquamars <kevin656504@hotmail.com>
2018-02-09 18:38:29 +0800

    remove space

--- codility/TapeEquilibrium/TapeEquilibrium.js
+++ codility/TapeEquilibrium/TapeEquilibrium.js
@@ -3,7 +3,7 @@
 
 function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
-        let sum = A.reduce((a,b)=>a+b,0);
+    let sum = A.reduce((a,b)=>a+b,0);
     let sumL = 0;
     let min = null;
     for(let i=0; i< A.length-1;i++){
