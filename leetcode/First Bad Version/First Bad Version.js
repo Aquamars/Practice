bd32399dd110637795d738624f3c989be4a26a57
Aquamars <kevin656504@hotmail.com>
2018-02-20 15:26:28 +0800

    add first bad version.js

---
+++ leetcode/First Bad Version/First Bad Version.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let min = 0;
+        let max = n;
+        let mid;
+        while(max > min){
+            mid = min+Math.floor((max-min)/2); 
+            if(isBadVersion(mid)){
+                max = mid;
+            } else {
+                min = mid+1;
+            }
+        }
+        return max;
+    };
+};
